a7c6b451d307cf7b6b50eb7a78f5d61b8c46c812
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2024-09-29 18:18:40 -0700

    change colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,13 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#006400',       // Deep Green
+        secondary: '#333333',     // Dark Gray
+        accent: '#F0F0F0',        // Light Gray
+        textLight: '#FFFFFF',     // White
+        textDark: '#000000',      // Black
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
